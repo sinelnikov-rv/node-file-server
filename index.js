9bc7013e9584f6cf4d272f6f1f1c3fbf96a2476d
Владимир Бойко <E>
2018-06-19 16:02:05 +0300

    add function fsreaddir

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-const http = require('http')
-const port = 3000
-const fs = require('fs')
-const path = require('path')
+const http = require('http');
+
+const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('hello')
-}
-
-const server = http.createServer(requestHandler)
+  console.log(request.url);
+  response.end('hello');
+};
+const getFiles = (dir, fileList) => {
+  //fileList = fileList || [];
+  var fileList = fs.readdirSync(dir);
+  console.log(fileList)
+  for (const i in fileList) {
+    const name = `${dir}/${fileList[i]}`;
+    
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, fileList);
+    } else {
+      fileList.push(name);
+    }
+  }
+  
+  return fileList;
+};
+getFiles('./')
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-    if(err) {
-        return console.log('error', err)
-    }
-    console.log(`server is listening on ${port}`)
-})
+  if (err) {
+    return console.log('error', err);
+  }
+  console.log(`server is listening on ${port}`);
+});
