ed5390f7948a5df9bee8c0059705502a1bf2a0df
Владимир Бойко <E>
2018-06-21 16:17:02 +0300

    add folders for test

--- index.js
+++ index.js
@@ -4,27 +4,37 @@ const port = 3000;
 const fs = require('fs');
 const path = require('path');
 
+const dir = './public';
+
+const getFiles = (directory) => {
+  const directoryList = [];
+  const fileList = fs.readdirSync(directory);
+  fileList.forEach((element) => {
+    if (fs.statSync(`${directory}/${element}`).isDirectory()) {
+      directoryList.push({ type: 'dir', name: `${element}`, parentDir: `${directory}` });
+      getFiles(`${directory}/${element}`);
+    } else {
+      directoryList.push({ type: 'file', name: `${element}`, parentDir: `${directory}` });
+    }
+  });
+  console.log(directoryList);
+  return directoryList;
+};
+
 const requestHandler = (request, response) => {
+  response.writeHead(200);
+  response.write(`<html><head><title>Directory listing for ${dir} </title><head><body><h1>Directory listing for ${dir}</h1>`);
   console.log(request.url);
+  const fileList = getFiles(dir);
+  response.write('<ul>');
+  fileList.forEach((element) => {
+    if (element.parentDir === dir) {
+      response.write(`<li> ${element.name} </li>`);
+    }
+  });
   response.end('hello');
 };
-const getFiles = (dir, fileList) => {
-  //fileList = fileList || [];
-  var fileList = fs.readdirSync(dir);
-  console.log(fileList)
-  for (const i in fileList) {
-    const name = `${dir}/${fileList[i]}`;
-    
-    if (fs.statSync(name).isDirectory()) {
-      getFiles(name, fileList);
-    } else {
-      fileList.push(name);
-    }
-  }
-  
-  return fileList;
-};
-getFiles('./')
+
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
